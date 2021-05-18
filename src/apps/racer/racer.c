6aa8e6c7fb7c2f34e1389588de9c1a7593bc22f0
Randipa Gunathilake <wrg20@uclive.ac.nz>
2021-05-19 11:29:31 +1200

    Radio data parsing and motor working togther

--- src/apps/racer/racer.c
+++ src/apps/racer/racer.c
@@ -4,6 +4,7 @@
 #include "motors.h"
 #include "radio_comm.h"
 #include "servos.h"
+#include "usb_comm.h"
 
 #include "stdlib.h"
 #include "string.h"
@@ -11,13 +12,15 @@
 void racer_init(void)
 {
     /* Configure LED PIO as output.  */
-    pio_config_set (LED1_PIO, PIO_OUTPUT_LOW);
-    pio_config_set (LED2_PIO, PIO_OUTPUT_HIGH);
+    pio_config_set (LED1_PIO, PIO_OUTPUT_HIGH);
+    pio_config_set (LED2_PIO, PIO_OUTPUT_LOW);
 
     /* Initilize the motors*/
     motors_init();
 
     radio_init();
+
+    usb_comm_init();
 
     pacer_init(LOOP_POLL_RATE);
 }
@@ -32,42 +35,57 @@ int main(void)
 
     char w_buff[32];
     char r_buff[12] = {0};
-    uint8_t mtr_l_speed;
-    uint8_t mtr_r_speed;
+    int radio_vals[12];
+    int16_t mtr_l_speed;
+    int16_t mtr_r_speed;
     sprintf(w_buff, "Ohh OOH\r\n");
 
     /* Initilize the car - GPIO, pacer and motors*/
     racer_init();
 
     while (1) {
+        pio_config_set (LED2_PIO, PIO_OUTPUT_HIGH);
         /* Wait until next clock tick.  */
         pacer_wait();
+
+        radio_read(r_buff, sizeof(r_buff));
+
+        //printf("BUFF:%s\n", r_buff);
+
+        int i = 0;
+        char *token = strtok(r_buff, ",");
+        // loop through the string to extract all other tokens
+        while (token != NULL)
+        {
+            radio_vals[i] = atoi(token);
+            token = strtok(NULL, ",");
+            i++;
+        }
+
+        mtr_r_speed = radio_vals[0] - 100;
+        mtr_l_speed = radio_vals[1] - 100;
+
+        motor_left_set(mtr_l_speed);
+        motor_right_set(mtr_r_speed);
+
+        printf("%d,%d\n", mtr_l_speed, mtr_r_speed);
 
         flash_ticks++;
         if (flash_ticks >= LOOP_POLL_RATE / (LED_FLASH_RATE * 2))
         {
             flash_ticks = 0;
             pio_output_toggle(LED1_PIO);
-            radio_write(w_buff, sizeof(w_buff));
-        }
-        count++;
-        if (count > 100)
-            count = 0;
+            //radio_write(w_buff, sizeof(w_buff));
+            count++;
+            if (count > 60)
+                count = 0;
 
-        radio_read(r_buff, sizeof(r_buff));
-
-        char *pch;
-        pch = strtok(r_buff, ",");
-        mtr_l_speed = atoi(pch);
-        motor_left_set(mtr_l_speed);
-        pch = strtok(NULL, ",");
-        mtr_r_speed = atoi(pch);
-        motor_right_set(mtr_r_speed);
-
+       }
+    
         radio_ticks++;
         if (radio_ticks >= LOOP_POLL_RATE / (RADIO_POLL_RATE * 2))
         {
-            radio_ticks = 0;
+           radio_ticks = 0;
         }
      }
 
--- src/apps/racer/usb_comm.c
+++ src/apps/racer/usb_comm.c
@@ -1,54 +1,16 @@
 #include "usb_comm.h"
+#include "usb_serial.h"
+#include "target.h"
 
-static void usb_comm_init(void)
+static usb_serial_cfg_t usb_serial_cfg =
 {
-    usb_cdc_t usb_cdc;
-    int flash_ticks = 0;
-    int i;
+    .read_timeout_us = 1,
+    .write_timeout_us = 1,
+};
 
-    // Create non-blocking tty device for USB CDC connection.
-    usb_serial = usb_serial_init(&usb_serial_cfg, "");
+void usb_comm_init(void){
+    usb_serial_init (&usb_serial_cfg, "/dev/usb_tty");
 
-    for (i = 0; i < 100; i++)
-    {
-        usb_serial_puts(usb_serial, "Hello world\n");
-        delay_ms(100);
-    }
-
-    prompt_command();
-}
-
-static void prompt_command(void)
-{
-    usb_serial_puts (usb_serial, "> ");
-}
-
-static void process_command(void)
-{
-    char buffer[80];
-    char *str;
-    
-    str = usb_serial_gets (usb_serial, buffer, sizeof (buffer));
-    if (! str)
-        return;
-
-    // usb_serial (usb_serial, "<<<%s>>>\n", str);
-    
-    switch (str[0])
-    {
-    case '0':
-        break;
-        
-    case '1':
-        break;
-
-    case 'h':
-        usb_serial_puts (usb_serial, "Hello world!\n");
-        break;
-
-    default:
-       break;
-    }
-
-    prompt_command ();
+    freopen ("/dev/usb_tty", "a", stdout);
+    freopen ("/dev/usb_tty", "r", stdin);
 }
