3ef6022229dd2a85bcc4494565f157d574031f7b
Hank Wu <jhw83@canterbury.ac.nz>
2021-05-25 15:36:33 +1200

    disable power reading for now. read power always returns 5v

--- src/apps/hat/joystick.h
+++ src/apps/hat/joystick.h
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 void joystick_power_sense_init(int pacer_rate);
 struct Command joystick_get_speed_command(void);
-void update_adc_and_button(void);
+void update_adc(void);
+void update_button(void);
 bool is_debug(void);
 bool go_sleep(void);
 bool joystick_button_pushed(void);
